a73300288afefad2ab2db8b4c24e0f7f428f56be
Richard Torcato <rtorcato@me.com>
2025-10-24 17:31:55 -0400

    fix: configure semantic-release for GitHub instead of GitLab

--- release.config.mjs
+++ release.config.mjs
@@ -1,3 +1,17 @@
-import semanticRelease from '@rtorcato/js-tooling/semantic-release'
-
-export default { ...semanticRelease }
+export default {
+	branches: ['main', 'master'],
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		'@semantic-release/changelog',
+		'@semantic-release/npm',
+		'@semantic-release/github',
+		[
+			'@semantic-release/git',
+			{
+				assets: ['CHANGELOG.md', 'package.json'],
+				message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+			},
+		],
+	],
+}
